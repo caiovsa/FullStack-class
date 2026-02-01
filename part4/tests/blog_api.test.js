0e7eb864d87f1d3ea8265ca383c810cdbc234126
Caio Vasconcelos Silva Andrade <caiovsa2@gmail.com>
2026-02-01 20:31:53 -0300

    Exercise 4.9

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -43,6 +43,16 @@ test('all blogs are returned', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+test('unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    assert.ok(blog.id)
+    assert.strictEqual(typeof blog.id, 'string')
+    assert.strictEqual(blog._id, undefined)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
